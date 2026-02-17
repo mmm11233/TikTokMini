a8cef1b96a18e64a8fb790a4572361b24de3543a
Mariam Joglidze <mariamjoglidze1@gmail.com>
2026-02-17 14:47:00 +0400

    Initial Commit

---
+++ TikTokMiniTests/TikTokMiniTests.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokMiniTests.swift
+//  TikTokMiniTests
+//
+//  Created by Mariam Joglidze on 17.02.26.
+//
+
+import Testing
+@testable import TikTokMini
+
+struct TikTokMiniTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
